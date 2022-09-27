f82ec36de4edea5024549cb9b5e9aad870197d72
Audrius Nen <audriusnen@gmail.com>
2022-09-27 22:03:45 +0300

    Create addToNumbers.cpp

---
+++ addToNumbersTeks-Edabit/addToNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>;
+
+using namespace std;
+
+
+int addition(int a, int b) {
+        return a + b;
+}
+
+int main() {
+    cout << "Sum [2+2] = " << addition(2,2) << endl;
+    return 0;
+}
