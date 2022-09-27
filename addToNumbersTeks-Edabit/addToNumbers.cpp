148267a6ff9f3cef97d6bd3eab700caa8a732d86
Audrius Nen <audriusnen@gmail.com>
2022-09-27 22:05:32 +0300

    Update addToNumbers.cpp

--- addToNumbersTeks-Edabit/addToNumbers.cpp
+++ addToNumbersTeks-Edabit/addToNumbers.cpp
@@ -3,11 +3,20 @@
 using namespace std;
 
 
+// Mano sum funkcija kuri sudeda du skaičius
+// ---- START ---- 
 int addition(int a, int b) {
         return a + b;
 }
 
+// ---- END ----
+
+
+// Mano pgr. funckija kuri suveikia paleidžiant programą
+// ---- START ---- 
 int main() {
     cout << "Sum [2+2] = " << addition(2,2) << endl;
     return 0;
 }
+
+// ---- END ----
